ec3cbfe9a1ebee7df4de71a73e0f1c10ea7eb259
Zikreddin <menzilguleri@gmail.com>
2026-07-19 17:15:25 +0300

    Update 09.sql

--- solutions/09.sql
+++ solutions/09.sql
@@ -1,10 +1,10 @@
--- Question: List the first name (FirstName), last name (LastName), and total
--- number of orders for customers who have placed more than 2 orders in total.
--- Sort from the highest order count to the lowest
+-- Question: List each category name (CategoryName) along with the total
+-- revenue it generated in 2026. Sort from the highest revenue to the lowest
 
-SELECT c.[FirstName], c.[LastName], COUNT(o.[CustomerID]) AS [Total]
-FROM [Customers] c
-INNER JOIN [Orders] o ON o.[CustomerID] = c.[CustomerID]
-GROUP BY c.[FirstName], c.[LastName]
-HAVING COUNT(o.[CustomerID]) > 2
+SELECT c.[CategoryName], SUM(p.[UnitPrice] * od.[Quantity]) AS [Total]
+FROM [Categories] c
+INNER JOIN [Products] p ON p.[CategoryID] = c.[CategoryID]
+INNER JOIN [OrderDetails] od ON od.[ProductID] = p.[ProductID]
+INNER JOIN [Orders] o ON o.OrderID = od.[OrderID] AND o.[OrderDate] >= '2026-01-01'
+GROUP BY c.[CategoryName]
 ORDER BY [Total] DESC;
